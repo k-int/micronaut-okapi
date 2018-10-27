602260b48ff9b3b6a3d1ab0d0836254b61fc7758
Ian Ibbotson <ianibbo@gmail.com>
2018-10-27 09:32:48 +0100

    Building

--- src/main/java/com/k_int/mn/okapi/OkapiTenantController.java
+++ src/main/java/com/k_int/mn/okapi/OkapiTenantController.java
@@ -7,31 +7,32 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import java.util.Map;
 import java.util.HashMap;
-import lombok.extern.slf4j.Slf4j
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Slf4j
-@CompileStatic
 @Controller("/_/tenant")
 public class OkapiTenantController {
 
+  private static final Logger LOG = LoggerFactory.getLogger(OkapiTenantController.class); 
+
   @Get
   public HttpResponse<?> getTenant() {
-    Map result = new HashMap()
-    
+    Map result = new HashMap();
+    LOG.debug("OkapiTenantController::getTenant");
     return HttpResponse.ok(result);
   }
 
   @Post 
   public HttpResponse<?> createTenant() {
-    Map result = new HashMap()
-
+    Map result = new HashMap();
+    LOG.debug("OkapiTenantController::createTenant");
     return HttpResponse.ok(result);
   }
 
   @Delete 
   public HttpResponse<?> destroyTenant() {
-    Map result = new HashMap()
-
+    Map result = new HashMap();
+    LOG.debug("OkapiTenantController::destroyTenant");
     return HttpResponse.ok(result);
   }
 
